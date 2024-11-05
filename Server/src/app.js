ac1ab49572c2b1bc250802f29c3320cd430dbff2
Abinash2003github <chowdhury.abinash2@gmail.com>
2024-11-05 21:09:21 +0530

    updated routes, app.js

--- Server/src/app.js
+++ Server/src/app.js
@@ -1,10 +1,21 @@
 import express from "express";
 import cookieParser from "cookie-parser";
+import path from "path";
+
+// Create a helper function to mimic __dirname
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "../../Frontend/crop_recommendation"));
 
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "../public")));
+// app.use(express.static(path.join(__dirname, '../../Frontend')));
 app.use(cookieParser());
 
 //routes
--- Server/src/routes/routes.js
+++ Server/src/routes/routes.js
@@ -1,6 +1,12 @@
 import { Router } from "express";
 const router = Router();
 
+import path from "path";
+// Create a helper function to mimic __dirname
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 import { createUser } from "../controllers/createUser.controller.js";
 import { logInUser } from "../controllers/logInUser.controller.js";
 import { logOutUser } from "../controllers/logOutUser.controller.js";
@@ -23,5 +29,23 @@ router
 //secure routes
 router.route("/logout").post(verifyJWT, logOutUser);
 
+//render template [use  "path.join(__dirname,"../../../Frontend")" to access files from Frontend ]
+router
+  .route("/crops_recommendation")
+  .get((req, res) => {
+    res.sendFile(
+      path.join(
+        __dirname,
+        "../../../Frontend/crop_recommendation/crop_recommendation.html"
+      )
+    );
+  })
+  .post((req, res) => {
+    const data = {
+      crop: "Rice",
+      growing_period: "18",
+    };
+    res.render("crop_recommendation-result", data);
+  });
 
 export default router;
