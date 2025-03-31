b945efa8e6317836234c298b5e7a62cfa45cea89
Abinash2003github <chowdhury.abinash2@gmail.com>
2025-03-31 17:07:35 +0530

    Updated controllers, scripts

--- Server/scripts/fertilizerRecommendation.py
+++ Server/scripts/fertilizerRecommendation.py
@@ -3,11 +3,18 @@ import sys
 
 try:
     # Parse arguments
+    # nitrogen = float(sys.argv[1])
+    # phosphorus = float(sys.argv[2])
+    # potassium = float(sys.argv[3])
+    # temperature = float(sys.argv[4])
+    # # humidity = float(sys.argv[5])
+    # crop_name = sys.argv[6]
+
     nitrogen = float(sys.argv[1])
     phosphorus = float(sys.argv[2])
     potassium = float(sys.argv[3])
-    temperature = float(sys.argv[4])
-    humidity = float(sys.argv[5])
+    ph = float(sys.argv[4]) 
+    temperature = float(sys.argv[5])
     crop_name = sys.argv[6]
 
     crop_dict={'Barley': 0, 'Cotton': 1, 'Ground Nuts': 2, 'Maize': 3, 'Millets': 4, 'Oil seeds': 5, 'Paddy': 6, 'Pulses': 7, 'Sugarcane': 8, 'Tobacco': 9, 'Wheat': 10}
@@ -17,7 +24,7 @@ try:
         model = pkl.load(file)
         
         # Prepare data as a 2D array
-        data = [[temperature, humidity, crop_dict[crop_name], nitrogen, phosphorus, potassium]]        
+        data = [[nitrogen, phosphorus, potassium, ph, temperature, crop_dict[crop_name]]]        
         
         # Predict
         output = model.predict(data)
--- Server/src/controllers/fertilizerRecommendation.controller.js
+++ Server/src/controllers/fertilizerRecommendation.controller.js
@@ -3,14 +3,16 @@ import ApiError from "../utils/ApiError.js";
 import runPy from "../utils/ApiRunPy.js";
 
 const fertilizerRecommendation = asyncHandler(async (req, res) => {
-  const { nitrogen, phosphorus, potassium, temperature, humidity, crop_name } = req.body;
-  
+  // const { nitrogen, phosphorus, potassium, temperature, humidity, crop_name } = req.body;
+  const { nitrogen, phosphorus, potassium, ph, temperature, crop_name } = req.body;  
+
   //check for required fields
   if (!nitrogen) throw new ApiError(401, "Enter the Nitrogen value");
   if (!phosphorus) throw new ApiError(401, "Enter the Phosphorus value");
   if (!potassium) throw new ApiError(401, "Enter the Potassium value");
+  if (!ph) throw new ApiError(401, "Enter the pH value");
   if (!temperature) throw new ApiError(401, "Enter the Temperature value");
-  if (!humidity) throw new ApiError(401, "Enter the Humidity value");
+  // if (!humidity) throw new ApiError(401, "Enter the Humidity value");
   if (!crop_name) throw new ApiError(401, "Enter the Crop name");
 
   //Connect and get data from Python Code within "script" by sending the inputs
@@ -18,8 +20,9 @@ const fertilizerRecommendation = asyncHandler(async (req, res) => {
     nitrogen,
     phosphorus,
     potassium,
+    ph,
     temperature,
-    humidity,
+    // humidity,
     crop_name,
   ]);
 
