d998675d928d0c024a2f38a4a3aa37ff798f6b21
Abinash2003github <chowdhury.abinash2@gmail.com>
2025-04-27 12:48:35 +0530

    Updated frontend, backend

--- Server/src/controllers/diseaseDetection.controller.js
+++ Server/src/controllers/diseaseDetection.controller.js
@@ -12,11 +12,12 @@ const __dirname = path.dirname(__filename);
 const diseaseDetection = asyncHandler(async (req, res) => {
   const imagePath = req.file.path
     .replace(/.*public/, ''); // Remove everything before /public
-
+  const des= "Apple Cedar Apple Rust is a fungal disease that affects apple trees and needs both apple and cedar (or juniper) trees to spread. It causes yellow-orange spots on apple leaves, which can lead to leaf damage and poor fruit growth. To control it, remove nearby cedar trees, use fungicides in early spring, prune infected parts, and plant disease-resistant apple varieties. Keeping trees healthy also helps protect them from the disease."
+  const cu="Managing Apple Cedar Apple Rust involves several steps. Removing nearby cedar or juniper trees can help break the disease cycle. Applying fungicides like myclobutanil or propiconazole during early spring protects new leaves from infection. Regular pruning of infected leaves and branches helps limit the spread. Planting rust-resistant apple varieties and maintaining healthy tree care practices, such as proper watering and fertilization, also strengthen the tree's natural defenses against the disease."
   const data = {
-    diseaseName: req.body.crop,
-    diseaseDescription: req.file.path,
-    diseaseCure: imagePath,
+    diseaseName: "Apple Cedar Apple Rust",
+    diseaseDescription: des,
+    diseaseCure: cu,
     diseaseImage: imagePath
   };
   //send respond
